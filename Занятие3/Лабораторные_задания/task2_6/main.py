d470893a1fed6825ffb64edc2ef2c6c5562acea4
pavsta2 <pavelstarshinov@gmail.com>
2021-11-24 21:19:19 +0300

    update 24.11 end of practice

--- Занятие3/Лабораторные_задания/task2_6/main.py
+++ Занятие3/Лабораторные_задания/task2_6/main.py
@@ -13,4 +13,8 @@ if __name__ == "__main__":
     data = task()
     print(json.dumps(data, indent=4))
 
+
+    with open("output.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
     #дополнительно записать отсортированный список в JSON файл
