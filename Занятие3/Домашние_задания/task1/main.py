0bad167ab9db9838e64693b16122b087a2146e7a
pavelstarshinov@gmail.com <pav$sta2>
2021-11-29 11:20:09 +0300

    29.11

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,33 +1,43 @@
+def read_file(input_file):
+    with open(input_file) as f:
+        result = f.read().split(",")
+        for i in range(len(result)):
+            for j in result[i]:
+                if not (j.isalpha() or j.isdigit()):
+                    result[i] = result[i].replace(j, '')
+    return result
+
+
+def final_list(list_str):
+    list_ok = []
+    for d in list_str:
+        if d.isdigit():
+            list_ok.append(int(d))
+        else:
+            list_ok.append(d)
+    return list_ok
+
+
 if __name__ == "__main__":
-    import numbers
 
     input_file_1 = "input1"
     input_file_2 = "input2"
     output_file = "output"
 
-    with open(input_file_1) as f:
-        result = f.read().split(",")
-        print(result, type(result))
-        for i in range(len(result)):
-            for j in result[i]:
-                if not (j.isalpha() or j.isdigit()):
-                    result[i] = result[i].replace(j, '')
+    list_str_1 = read_file(input_file_1)
+    print(list_str_1, type(list_str_1))
 
-        print(result, type(result))
+    list_str_2 = read_file(input_file_2)
+    print(list_str_2, type(list_str_2))
 
-    with open(input_file_2) as f:
-        result2 = f.read().split(",")
-        print(result2, type(result2))
-        for i in range(len(result2)):
-            for j in result2[i]:
-                if not (j.isdigit() or j.isalpha()):
-                    result2[i] = result2[i].replace(j, '')
-        print(result2, type(result2))
+    list_dig_str_1 = final_list(list_str_1)
+    list_dig_str_2 = final_list(list_str_2)
 
-    final_list = [int(d) for d in result if d.isdigit()] + [int(d) for d in result2 if d.isdigit()]
-    print(final_list)
+    print(list_dig_str_1)
+    print(list_dig_str_2)
 
-    with open(output_file,"w",encoding= "UTF-8") as f:
-        f.write(str(final_list))
+    final_list_dig_str = list_dig_str_1 + list_dig_str_2
+    print(final_list_dig_str)
 
-
+    with open(output_file, "w", encoding="UTF-8") as f:
+        f.write(str(final_list_dig_str))
